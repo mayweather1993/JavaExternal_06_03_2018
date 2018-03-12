5c96c0892eaff4c9fdb2129217675d6401e11117
mayweather <spoxer@bigmir.net>
2018-03-12 22:36:36 +0200

    added rhombus

--- lesson_01/task_01/Rhombus.java
+++ lesson_01/task_01/Rhombus.java
@@ -2,12 +2,28 @@ package task_01;
 
 public class Rhombus {
     public static void main(String[] args) {
-
+       printRhombus(7);
     }
-
-    /////in progress
-    public static void printRhombus(int diagonal) {
-
+    public static void printRhombus(int length){
+        for (int i = 0; i < length; i++) {
+            for (int j = i; j < length ; j++) {
+                System.out.print(" ");
             }
+            for (int k = length - i - 1; k < length; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
         }
+        for (int i = 1; i < length; i++) {
+            for (int j = length - i - 1; j < length; j++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k < length; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
 
