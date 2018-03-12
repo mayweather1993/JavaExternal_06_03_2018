408cfa581ef1d8131276a75214f681278064e628
mayweather <spoxer@bigmir.net>
2018-03-12 02:28:27 +0200

    need to do

--- lesson_01/task_01/Rhombus.java
+++ lesson_01/task_01/Rhombus.java
@@ -5,13 +5,9 @@ public class Rhombus {
 
     }
 
+    /////in progress
     public static void printRhombus(int diagonal) {
-        for (int i = diagonal / 2; i >= diagonal; i--) {
-            for (int j = 0; j < i; j++) {
-                System.out.print(" ");
-            }
-            for (int k = 1; k <= (diagonal - i * 2) && i >= 0 ; k++) {
-                
+
             }
         }
     }
