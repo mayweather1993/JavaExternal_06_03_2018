90317dd3d0dd4bc9d49ce46baeee6b002a3ac91f
mayweather <spoxer@bigmir.net>
2018-03-19 20:36:19 +0200

    Small refactoring

--- lesson_02/Drawer_task1.java
+++ lesson_02/Drawer_task1.java
@@ -7,7 +7,7 @@ public class Drawer_task1 {
         figures.add(new Point(11, 18));
         figures.add(new Point(3, 10));
         figures.add(new Line(new Point(4, 5), new Point(9, 3)));
-        figures.add(new Triangle((new Point(7, 8)), new Point(5, 6), new Point(9, 15)));
+        figures.add(new Triangle(new Point(7, 8), new Point(5, 6), new Point(9, 15)));
         figures.add(new Rectangle(new Point(2,3),new Point(2,6), new Point(6,6), new Point(6,3)));
         System.out.println(figures);
     }
