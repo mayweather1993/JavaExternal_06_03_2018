ecd4b41bb69cefb0b5aeaeb59642a0f9d3fed56f
mayweather <spoxer@bigmir.net>
2018-03-19 23:29:59 +0200

    batmobile + amphi

--- lesson_04/entities/Amphibian.java
+++ lesson_04/entities/Amphibian.java
@@ -1,4 +1,14 @@
 package entities;
 
-public class Amphibian {
+import dao.ISwim;
+
+public class Amphibian extends CCar implements ISwim {
+    public Amphibian(double price, int year, double speed, int x, int y) {
+        super(price, year, speed, x, y);
+    }
+
+    @Override
+    public void swim() {
+
+    }
 }
--- lesson_04/entities/Batmobile.java
+++ lesson_04/entities/Batmobile.java
@@ -1,7 +1,15 @@
 package entities;
 
-public class Batmobile extends CCar {
+import dao.IFly;
+
+public class Batmobile extends Amphibian implements IFly {
+
     public Batmobile(double price, int year, double speed, int x, int y) {
         super(price, year, speed, x, y);
+    }
+
+    @Override
+    public void fly() {
+
     }
 }
