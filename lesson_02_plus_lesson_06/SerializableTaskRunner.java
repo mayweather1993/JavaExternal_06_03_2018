b448f8916f498f6fa8fc5c6ce1e34a3b8ec33fde
mayweather <spoxer@bigmir.net>
2018-03-31 19:19:31 +0300

    finished lesson 6

--- lesson_02_plus_lesson_06/SerializableTaskRunner.java
+++ lesson_02_plus_lesson_06/SerializableTaskRunner.java
@@ -11,18 +11,18 @@ public class SerializableTaskRunner {
         Point pointA = new Point(2, 2);
         Point pointB = new Point(1, 7);
 
-        Line line = new Line(pointA, pointB);
+        Drawable line = new Line(pointA, pointB);
 
-        Triangle triangle = new Triangle(p1, pointB, p2);
-        Rectangle rectangle = new Rectangle(p1, p2, pointA, pointB);
+        Drawable triangle = new Triangle(p1, pointB, p2);
+        Drawable rectangle = new Rectangle(p1, p2, pointA, pointB);
 
-        Polygon poli2 = new Polygon(new Point(10, 10), new Point(17, 2), new Point(14, 13), new Point(2, 0), new Point(4, -34));
+        Polygon polygon = new Polygon(new Point(10, 10), new Point(17, 2), new Point(14, 13), new Point(2, 0), new Point(4, -34));
 
-        poli2.putFigure(p1);
-        poli2.putFigure(p2);
-        poli2.putFigure(line);
-        poli2.putFigure(triangle);
-        poli2.putFigure(rectangle);
+        polygon.putFigure(p1);
+        polygon.putFigure(p2);
+        polygon.putFigure(line);
+        polygon.putFigure(triangle);
+        polygon.putFigure(rectangle);
 
         List<Drawable> drawables = Arrays.asList(triangle, pointA, p1);
         String fileName = "drawing.txt";
@@ -30,9 +30,20 @@ public class SerializableTaskRunner {
         for (Drawable drawable : drawables) {
             System.out.println("Before serialize: " + drawable.toString());
             SerializableUtils.serializeFigures(fileName, drawable);
-            Serializable triangleDesiarilized = SerializableUtils.deserializeFigures(fileName, Serializable.class);
-            System.out.println("After desirialize: " + triangleDesiarilized);
-        }
+            Serializable triangleDeserialize = SerializableUtils.deserializeFigures(fileName, Serializable.class);
+            System.out.println("After deserialize: " + triangleDeserialize + "\n");
 
+        }
+        SerializableUtils.serializeFigures("triangle.txt", triangle);
+        SerializableUtils.serializeFigures("rectangle.txt", rectangle);
+        SerializableUtils.serializeFigures("line.txt", line);
+        System.out.println("Deserialized triangle :");
+        System.out.println(SerializableUtils.deserializeFigures("triangle.txt", Serializable.class));
+        System.out.println("\n");
+        System.out.println("Deserialized rectangle :");
+        System.out.println(SerializableUtils.deserializeFigures("rectangle.txt", Serializable.class));
+        System.out.println("\n");
+        System.out.println("Deserialized line :");
+        System.out.println(SerializableUtils.deserializeFigures("line.txt", Serializable.class));
     }
 }
